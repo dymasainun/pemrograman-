51dce8c4d05bd5a053829cb3f0f0ac9856e9f747
dymasainun <nundymas@gmail.com>
2017-05-04 15:51:51 +0700

    first commit

---
+++ Luas/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double jejari, luas, PI=3.14;
+    cout << "Masukkan jari-jari lingkaran : " ;
+    cin >> jejari;
+    luas = PI * jejari * jejari;
+    cout << "Luas lingkaran : " << luas;
+}
